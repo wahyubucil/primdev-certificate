f3b4f78afa7c5fd67f8d220c3f2f4902fb1772dc
Wahyu Bucil <wahyubucil@gmail.com>
2021-07-24 22:28:51 +0800

    Add chai-bn

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,11 @@ import { task, HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
+import chaiBn from "chai-bn";
+import BN from "bn.js";
 
 chai.use(chaiAsPromised);
+chai.use(chaiBn(BN));
 
 task("accounts", "Prints the list of accounts", async (_, hre) => {
   const accounts = await hre.ethers.getSigners();
