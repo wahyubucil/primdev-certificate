0dc376b7894630dd4a610ddd81b70e2824618c53
Wahyu Bucil <wahyubucil@gmail.com>
2021-07-29 10:31:24 +0800

    Complete dashboard header

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import App from './App';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,49 @@
+import { Avatar, Col, Dropdown, Layout, Menu, Row } from 'antd';
+import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
 import type { FC } from 'react';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.scss';
+import logo from '@/assets/logo-primakara-developers.svg';
 
-export const Dashboard: FC = () => <h1>Dashboard</h1>;
+const { Header, Content, Footer } = Layout;
+
+const menu = (
+  <Menu>
+    <Menu.Item icon={<LogoutOutlined />}>
+      <a onClick={() => console.log('Logout')}>Logout</a>
+    </Menu.Item>
+  </Menu>
+);
+
+export const Dashboard: FC = () => (
+  <Layout>
+    <Header className="Dashboard__header">
+      <Row className="full-height">
+        <Col flex="auto" className="full-height">
+          <Link to="/dashboard">
+            <img
+              src={logo}
+              alt="Logo Primakara Developers"
+              className="full-height"
+            />
+          </Link>
+        </Col>
+        <Col>
+          <Dropdown overlay={menu} placement="bottomRight">
+            <div className="Dashboard__header-menu">
+              <Avatar className="Dashboard__avatar" icon={<UserOutlined />} />
+              <span>Administrator</span>
+            </div>
+          </Dropdown>
+        </Col>
+      </Row>
+    </Header>
+    <Content style={{ padding: '50px' }}>
+      <div className="site-layout-content">Content</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+      Ant Design ©2018 Created by Ant UED
+    </Footer>
+  </Layout>
+);
