c449430529acce1bf7ff2090efb3cb1e398dce5d
Wahyu Bucil <wahyubucil@gmail.com>
2021-07-30 11:46:12 +0800

    Complete validity checker page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+import { CopyrightOutlined, GithubOutlined } from '@ant-design/icons';
+import React, { VFC } from 'react';
+import './Footer.scss';
+
+export const Footer: VFC<{ withPadding: boolean }> = ({
+  withPadding = true,
+}) => (
+  <div className={withPadding ? 'Footer Footer--with-padding' : 'Footer'}>
+    <div className="Footer__links">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/primakara-developers"
+      >
+        Primakara Developers
+      </a>
+      <a
+        title="github"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/wahyubucil/primdev-certificate"
+      >
+        <GithubOutlined />
+      </a>
+      <a target="_blank" rel="noopener noreferrer" href="https://nextap.co">
+        Nextap &amp; Company
+      </a>
+    </div>
+    <div>
+      <CopyrightOutlined /> {new Date().getFullYear()} Created by{' '}
+      <a
+        href="https://wahyubucil.web.app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Wahyu Budi Saputra
+      </a>
+    </div>
+  </div>
+);
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,54 @@
 import React, { VFC } from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Form, Input, Result, Typography } from 'antd';
+import logo from '@/assets/logo-primakara-developers.svg';
+import { Footer } from '@/components/Footer';
+import './index.scss';
+
+const { Title, Paragraph, Text } = Typography;
 
 const Home: VFC = () => (
-  <>
-    <h1>Home</h1>
-    <Link to="/dashboard">To Dashboard</Link>
-  </>
+  <div className="Home">
+    <img src={logo} alt="Logo Primakara Developers" className="Home__logo" />
+    <div className="Home__content">
+      <Title level={3} className="Home__title">
+        Certificate Validity Checker
+      </Title>
+      <Form className="Home__form" layout="vertical" requiredMark={false}>
+        <Form.Item label="Code" name="code" rules={[{ required: true }]}>
+          <Input placeholder="Insert code from the certificate" />
+        </Form.Item>
+        <Form.Item
+          label="Participant Name"
+          name="name"
+          rules={[{ required: true }]}
+        >
+          <Input placeholder="Insert participant name on the certificate" />
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit" type="primary" block>
+            Check
+          </Button>
+        </Form.Item>
+      </Form>
+      <Result status="success" title="Certificate Valid" style={{ padding: 0 }}>
+        <Paragraph>
+          <Text strong style={{ fontSize: 16 }}>
+            Certificate Information
+          </Text>
+        </Paragraph>
+        <Paragraph>
+          <Text strong>Name :</Text> Workshop HTML
+        </Paragraph>
+        <Paragraph>
+          <Text strong>Expired :</Text> None
+        </Paragraph>
+        <Paragraph>
+          <Text strong>Created :</Text> 22 July 2022
+        </Paragraph>
+      </Result>
+    </div>
+    <Footer />
+  </div>
 );
 
 export default Home;
